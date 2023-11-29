c8943179437cc4670893521c20c2a762d9ce9b24
Gaetan Schneller <gaetan.schneller@onera.fr>
2023-11-29 10:12:21 +0100

    WIP

--- textIntegrityInspector/__init__.py
+++ textIntegrityInspector/__init__.py
@@ -3,4 +3,4 @@ all = ('__version__',)
 from pbr.version import VersionInfo
 
 # Check the PBR version module docs for other options than release_string()
-__version__ = VersionInfo('<my_package>').release_string()
+__version__ = VersionInfo('textIntegrityInspector').release_string()
